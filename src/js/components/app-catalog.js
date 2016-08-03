7fdf550fdeded03209a3187909bad1879b5474bb
5656 <chensok@gmail.com>
2016-08-03 11:48:24 +0200

    higher order componenets

--- src/js/components/app-catalog.js
+++ src/js/components/app-catalog.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import AppStore from '../stores/app-store';
 import CatalogItem from './app-catalogitem';
+import StoreWatchMixin from '../mixins/StoreWatchMixin';
 
 //Get state of Catalog from the Store
 function getCatalog(){
     return {items: AppStore.getCatalog() }
 }
 
-class Catalog extends React.Component {
-    constructor(){
-        super(); //get This of the class
-        this.state = getCatalog()
-    }
-    render(){
-        let items = this.state.items.map( item => {
+const Catalog = (props) => {
+        let items = props.items.map( item => {
             return <CatalogItem key={ item.id } item={ item } />
         });
         return (
@@ -21,7 +17,6 @@ class Catalog extends React.Component {
                 {items }
             </div>
         )
-    }
 }
 
-export default Catalog;
+export default StoreWatchMixin(Catalog,getCatalog);
